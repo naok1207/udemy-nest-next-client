da857cbcf9922996ff24f129264161f368b0e3c9
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2023-02-11 16:19:03 +0900

    initial commit

---
+++ hooks/useMutateTask.ts
@@ -0,0 +1,94 @@
+import { Task } from '@prisma/client'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import useStore from '../store'
+import { EditedTask } from '../types'
+
+const useMutateTask = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  const reset = useStore((state) => state.resetEditedTask)
+
+  const createTaskMutation = useMutation(
+    // データ作成時にはserverでidを割り振るためidは不要
+    async (task: Omit<EditedTask, 'id'>) => {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/todo`,
+        task
+      )
+      return res.data
+    },
+    {
+      onSuccess: (res) => {
+        const previousTodos = queryClient.getQueryData<Task[]>(['tasks'])
+        if (previousTodos) {
+          queryClient.setQueryData(['tasks'], [res, ...previousTodos])
+        }
+        reset()
+      },
+      onError: (err: any) => {
+        reset()
+        if (err.response.status === 401 || err.response.status === 403) {
+          router.push('/')
+        }
+      },
+    }
+  )
+  const updateTaskMutation = useMutation(
+    async (task: EditedTask) => {
+      const res = await axios.patch(
+        `${process.env.NEXT_PUBLIC_API_URL}/todo/${task.id}`,
+        task
+      )
+      return res.data
+    },
+    {
+      onSuccess: (res, variables) => {
+        const previousTodos = queryClient.getQueryData<Task[]>(['tasks'])
+        if (previousTodos) {
+          queryClient.setQueryData(
+            ['tasks'],
+            previousTodos.map((task) => (task.id === res.id ? res : task))
+          )
+        }
+        reset()
+      },
+      onError: (err: any) => {
+        reset()
+        if (err.response.status === 401 || err.response.status === 403) {
+          router.push('/')
+        }
+      },
+    }
+  )
+  const deleteTaskMutation = useMutation(
+    // 引数が variables に対応している
+    async (id: number) => {
+      await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/todo/${id}`)
+    },
+    {
+      onSuccess: (_, variables) => {
+        const previousTodos = queryClient.getQueryData<Task[]>(['tasks'])
+        if (previousTodos) {
+          queryClient.setQueryData(
+            ['tasks'],
+            // variables は async (id: number) => {} の 引数 に対応している
+            previousTodos.filter((task) => task.id !== variables)
+          )
+        }
+        reset()
+      },
+      onError: (err: any) => {
+        reset()
+        if (err.response.status === 401 || err.response.status === 403) {
+          router.push('/')
+        }
+      },
+    }
+  )
+
+  return { createTaskMutation, updateTaskMutation, deleteTaskMutation }
+}
+
+export default useMutateTask
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,51 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import axios from 'axios'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false, // fetchが失敗した際に3回まで再fetchするのを無効化
+      refetchOnWindowFocus: false, // ブラウザをフォーカスした際にrefetchを行うのを無効化
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // cookieのやり取りを行う場合には true にする必要がある
+  axios.defaults.withCredentials = true
+  useEffect(() => {
+    // アプリを開いた際にcsrfTokenがheaderに自動で付与されるようになる
+    const getCsrfToken = async () => {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`
+      )
+      axios.defaults.headers.common['csrf-token'] = data.csrfToken
+    }
+    getCsrfToken()
+  }, [])
+
+  return (
+    // フロジェクト全体で react-query を使えるようにするための Provider
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+          fontFamily: 'Verdana, sans-serif',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+      {/* react-query の devtools */}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
